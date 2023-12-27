fa6c10b0508a4fdc0498bc3ba6a16a9171ed9087
2021331064 <137719695+2021331064@users.noreply.github.com>
2023-12-27 07:18:04 +0600

    Update now lexer.h

--- lexer.h
+++ lexer.h
@@ -22,6 +22,6 @@ typedef struct {
 void print_token(Token token);
 Token *generate_number(char *current,int *current_index);
 Token *generate_keyword(char *current,int *current_index);
-Token *generate_separator_or_operator(char *current,int *current_index);
+Token *generate_separator_or_operator(char *current,int *current_index,TokenType type);
 Token *lexer(FILE *file);
 #endif
